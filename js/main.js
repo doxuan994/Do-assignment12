ae71d021e27bfaf3a3bf667ed8fec9b2a7ecef34
Xuan Do <doxuancntp@gmail.com>
2019-04-26 18:47:47 -0400

    Commit.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Move sidebar on scroll
 let sidebar = document.getElementById('sidebar');
-let sidebarChild = sidebar.children[0];
+// let sidebarChild = sidebar.children[0];
+let navbar = document.getElementById('navbar');
 let footer = document.getElementById('footer');
 // let offsetTop = sidebar.offsetTop;
 
@@ -8,21 +9,18 @@ let footer = document.getElementById('footer');
 let header = document.getElementById('header');
 let headeroffsetHeight = header.offsetHeight;
 
+
 let windowInnerWidth = window.innerWidth;
 window.addEventListener('scroll', function() {
     let footerClientY = footer.getBoundingClientRect().top;
     let footerClientHeight = footer.clientHeight;
-
-
-    // Stop adding margin to sidebar when users reach the end of the content.
-    let stopAddMargin = footerClientY - footerClientHeight;
-
-    console.log(pageYOffset);
-
-    if (windowInnerWidth >= 1200 && pageYOffset > headeroffsetHeight  && stopAddMargin > 0) {
-        // Add data-spy="affix" to sidebar ul.
-        sidebarChild.classList.add("affix");
+    // Stop adding the affix class to navbar when users reach the end of the content.
+    let meetFooter = footerClientY - footerClientHeight;
+    
+    if (windowInnerWidth >= 1200 && pageYOffset > headeroffsetHeight  && meetFooter > 0) {
+        // Add data-spy="affix" to navbar.
+        navbar.classList.add("affix");
     } else {
-        sidebarChild.classList.remove("affix");
+        navbar.classList.remove("affix");
     }
 }, false);
